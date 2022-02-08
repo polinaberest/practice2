02b5c416f00816f9a0be1a93431e5b94816372f6
Polina Berest <97687804+polinaberest@users.noreply.github.com>
2022-02-08 19:58:49 +0200

    Add files via upload

---
+++ Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parse
+{
+    static class Extensions
+    {
+        public static long forGB = 1073741824;
+        public static long forMB = 1048576;
+
+        public static long ConvertSize(string initSize)
+        {
+            long transformedSize = 0;
+            if (initSize.Contains("GB"))
+            {
+                transformedSize = long.Parse(initSize.Substring(0, initSize.IndexOf("GB"))) * forGB;
+            }
+            else if (initSize.Contains("MB"))
+            {
+                transformedSize = long.Parse(initSize.Substring(0, initSize.IndexOf("MB"))) * forMB;
+            }
+            else if (initSize.Contains("B"))
+            {
+                transformedSize = long.Parse(initSize.Substring(0, initSize.IndexOf("B")));
+            }
+            return transformedSize;
+        }
+
+    }
+}
---
+++ Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parse
+{
+    class Image
+    {
+        public string extensionI = ".bmp";
+        public string Name { get; private set; }
+        public long Size { get; private set; }
+        public string Resolution { get; private set; }
+        
+        public Image(string name, string size, string resolution)
+        {
+            Name = name;
+            Size = Extensions.ConvertSize(size);
+            Resolution = resolution;
+        }
+    }
+}
